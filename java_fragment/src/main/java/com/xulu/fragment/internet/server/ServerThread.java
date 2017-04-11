01c0302b99f8ab514464709bfb76a555d9d99633
MonkeyCoderXu <xl103521010@163.com>
2017-04-11 11:15:45 +0800

    Update ServerThread.java

--- java_fragment/src/main/java/com/xulu/fragment/internet/server/ServerThread.java
+++ java_fragment/src/main/java/com/xulu/fragment/internet/server/ServerThread.java
@@ -1,1 +1,28 @@
 
+while(true) {  
+
+02     Socket socket = server.accept();   
+
+03     new Thread() {  
+
+04         public void run() {  
+
+05             InputStream in = socket.getInputStream();  
+
+06             while(true) {//input逻辑  
+
+07                 in.read(...)  
+
+08             }     
+
+09         }    
+
+10     }.start()     
+
+11     new Thread() {  
+
+12             //类似input  
+
+13       }.start()   
+
+14 } 
