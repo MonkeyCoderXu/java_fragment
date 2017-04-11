06e8b784056b77300624fff5d0b3c75b20c5e64c
MonkeyCoderXu <xl103521010@163.com>
2017-04-11 10:41:58 +0800

    Update SimpleServer.java

--- java_fragment/src/main/java/com/xulu/fragment/internet/server/SimpleServer.java
+++ java_fragment/src/main/java/com/xulu/fragment/internet/server/SimpleServer.java
@@ -1,5 +1,25 @@
 package com.xulu.fragment.internet.server;
 
-public class SimpleServer {
-
+public class SimpleServer 
+{
+  private ServerSocket server;
+  private Socket socket;
+  private InputStream is;
+  private OutputStream os;
+  
+  public SimpleServer(int serverPort)
+  {
+    this.server = new ServerSocket(serverPort);
+    socket = this.server.accept();
+  }
+  
+  public void readFromClient()
+  {
+    is = socket.getInputStream();
+  }
+  
+  public void writeToClient()
+  {
+    os = socket.getOutputStream();
+  }
 }
